52ce56c192f4a5b93b3cf50a1a331f13fa479619
OokuraYota <0912yota0820@gmail.com>
2020-12-09 02:40:44 +0900

    RaycastでEnemyのAIを作成したが、うまくいかなかったのでコメントを大量に書いたScriptを制作

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -17,6 +17,8 @@ public class EnemyMove : MonoBehaviour
 
     private NavMeshAgent _agent;
 
+    //private RaycastHit[] _raycastHits = new RaycastHit[10];
+
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();//NavMeshAgentを保持しておく
@@ -34,6 +36,35 @@ public class EnemyMove : MonoBehaviour
         //検知したオブジェクトに『 Player 』のタグが付いていれば、そのオブジェクトを追いかける
         if (collider.CompareTag("Player"))
         {
+            //時針とプレイヤーの座標差分を計算
+            //var positionDiff = collider.transform.position - transform.position;
+
+            //プレイヤーとの距離を計算
+            //var distance = positionDiff.magnitude;
+
+            //プレイヤーの方向
+            //var direction = positionDiff.normalized;
+
+            //raycastHitに、ヒットしたColliderや座標情報などが格納される
+            //RaycastAllとRaycastAllocは同等の機能だが、RaycastNonAllocだとメモリにゴミが残らないのでこちらを推奨
+            //var hitCount = Physics.RaycastNonAlloc(transform.position, direction, _raycastHits, distance);
+            //Debug.Log("hitCount :" + hitCount);
+            /*if (hitCount == 0)
+            {
+                //本作のプレイヤーは今のところColliderを使っていないのでRaycastはヒットしない
+                //つまり、ヒット数が0であればプレイヤーとの間に障害物がないということになる
+                _agent.isStopped = false;
+
+                //AI.NavMeshAgent - destination - 
+                //要求された目的地に最も近い有効な NavMesh の位置にエージェントを移動させることを要求します。
+                _agent.destination = collider.transform.position;
+            }
+            else
+            {
+                //見失ったら停止する
+                _agent.isStopped = true;
+            }*/
+
             //AI.NavMeshAgent - destination - 
             //要求された目的地に最も近い有効な NavMesh の位置にエージェントを移動させることを要求します。
             _agent.destination = collider.transform.position;
