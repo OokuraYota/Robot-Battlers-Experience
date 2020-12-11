64462fa4b167fc15e8369782e4a81e82d38a7992
OokuraYota <0912yota0820@gmail.com>
2020-12-12 04:49:57 +0900

    ・SwordAttackColliderのコメント追加

--- Assets/Scripts/SwordAttackCollider.cs
+++ Assets/Scripts/SwordAttackCollider.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// SwordのBoxColliderとEnemyのBoxColliderの重なり判定
+/// </summary>
 public class SwordAttackCollider : MonoBehaviour
 {
-    //剣
-    //重なり瞬間判定
+    /// <summary>
+    /// オブジェクト同士が重なった『瞬間』に呼び出される
+    /// </summary>
+    /// <param name="other">当たったColliderオブジェクトの情報</param>
     private void OnTriggerEnter(Collider other)
     {
         //関数名：OnTriggerEnter
@@ -13,27 +18,35 @@ public class SwordAttackCollider : MonoBehaviour
         //引数：Collider:当たったColliderオブジェクトの情報
         //内容：オブジェクト同士が重なった瞬間に呼び出される
 
-        Debug.Log("Swordが" + other.name + "Colliderに重なった");
+        Debug.Log("Swordが" + other.name + "Colliderに重なった！");
         //Debug.Log("") → Swordが + EnemyのBox + Colliderに重なった
     }
 
-    //重なり中の判定
+    /// <summary>
+    /// オブジェクト同士が重なっている『間』呼び出される
+    /// </summary>
+    /// <param name="other">当たったColliderオブジェクトの情報</param>
     private void OnTriggerStay(Collider other)
     {
         //関数名：OnTriggerStay
         //戻り値：なし
         //引数：Collider:当たったColliderオブジェクトの情報
         //オブジェクト同士が重なっている間呼び出される
-        Debug.Log(other.name + "Stay");
+
+        Debug.Log("Swordが" + other.name + "Colliderに重なっています！");
     }
 
-    //重なり離脱の判定
+    /// <summary>
+    /// 重なり合ったオブジェクト同士が『離れた瞬間』呼び出される
+    /// </summary>
+    /// <param name="other">当たったColliderオブジェクトの情報</param>
     private void OnTriggerExit(Collider other)
     {
         //関数名：OnTriggerExit
         //戻り値：なし
         //引数：Collider:当たったColliderオブジェクトの情報
         //内容：重なり合ったオブジェクト同士が離れた瞬間呼び出される
-        Debug.Log(other.name + "Exit");
+
+        Debug.Log("Swordが" + other.name + "Colliderから、今離れました！");
     }
 }
