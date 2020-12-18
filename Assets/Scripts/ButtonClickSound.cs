05c9086c4c4c61a804108ab7836368d2cbe8c5af
OokuraYota <0912yota0820@gmail.com>
2020-12-19 06:01:58 +0900

    2020/12/19 6:00 ・Timelineで詰まっちゃってるから、簡単なTitleのStartButtonにSoundを付けました。 ・Scene移動が速いので全然聞こえない。

---
+++ Assets/Scripts/ButtonClickSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Buttonを押したときのSound
+/// </summary>
+public class ButtonClickSound : MonoBehaviour
+{
+    private AudioSource STRAT;
+
+    void Start()
+    {
+        STRAT = GetComponent<AudioSource>();
+    }
+
+    public void OnClick()
+    {
+        STRAT.PlayOneShot(STRAT.clip);
+    }
+}
+
