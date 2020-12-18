3762884b5a414c0218bdbf45ca690b2b371386f7
OokuraYota <0912yota0820@gmail.com>
2020-12-19 03:30:26 +0900

    2020/12/19 3:27 ・クリックでボタンを押そうとしたが、剣攻撃で割り当てられているのでButtonをおしてTimelineを呼び出そうとした。 ・無事Timelineは呼び出せるようになったが、最後の視点から戻らなくなってしまう。 ・さらに一日中Timelineをやっていたのだが、カメラが相対座標？の方でやるとおかしくなってしまうので、絶対座標でやった。

---
+++ Assets/Scripts/TimelineSpecialMoveManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+/// <summary>
+/// Timelineの必殺技関係を管理
+/// </summary>
+public class TimelineSpecialMoveManager : MonoBehaviour
+{
+    //ここにインスペクター上であらかじめ複数のタイムラインアセットをセット
+    [SerializeField]
+    private PlayableDirector playableDirector;
+
+    /*public void Update()
+    {
+        OnClick();
+    }*/
+
+    /// <summary>
+    /// クリックした時Timelineを再生する
+    /// </summary>
+    /*public void OnClick()
+    {
+        Debug.Log("必殺技Buttonが押されました");
+        playableDirector.Play();
+    }*/
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("必殺技Buttonが押されました");
+            playableDirector.Play();
+        }   
+    }
+
+    /*public void Start()
+    {
+        this.playableDirector = this.GetComponent<PlayableDirector>();   
+    }
+
+    //イベント再生メソッド
+    void EventPlay(int id)
+    {
+        switch (id)
+        {
+            case 1:
+                //再生したいTimelineをPlayableDirectorに再生させる
+                this.playableDirector.Play();
+                break;
+            //case 2:
+        }
+    }*/
+
+}
