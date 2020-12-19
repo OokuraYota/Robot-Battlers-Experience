59cc1dac0e670e31376678ec91a4a656130b6c44
OokuraYota <0912yota0820@gmail.com>
2020-12-19 16:40:10 +0900

    2020/12/19 16:38 ・絶対座標のTimelineにした ・Gを押したらTimelineを再生 ・再生が終わったら元の位置に戻すスクリプトを記述

--- Assets/Scripts/TimelineSpecialMoveManager.cs
+++ Assets/Scripts/TimelineSpecialMoveManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityStandardAssets.Vehicles.Aeroplane;
 
 /// <summary>
 /// Timelineの必殺技関係を管理
@@ -13,45 +14,66 @@ public class TimelineSpecialMoveManager : MonoBehaviour
     [SerializeField]
     private PlayableDirector playableDirector;
 
-    /*public void Update()
-    {
-        OnClick();
-    }*/
+    Vector3 PlayerPosition;
 
-    /// <summary>
-    /// クリックした時Timelineを再生する
-    /// </summary>
-    /*public void OnClick()
-    {
-        Debug.Log("必殺技Buttonが押されました");
-        playableDirector.Play();
-    }*/
+    Vector3 PlayerRotation;
 
     void Update()
     {
+        //もし、キーボードのGを押したら
         if (Input.GetKeyDown(KeyCode.G))
         {
+            /* 指定した名前のオブジェクトを存在するすべてのアクティブな
+             * オブジェクトの中から探し出して取得する。
+             */
+            Vector3 PlayerPositon = GameObject.Find("PlayerRobot").transform.position;
+            Debug.Log("プレイヤーの位置は : " + PlayerPosition);
+
+            //角度も取得
+            Vector3 PlayerRotation = GameObject.Find("PlayerRobot").transform.eulerAngles;
+            Debug.Log("プレイヤーの角度は" + PlayerRotation);
+
             Debug.Log("必殺技Buttonが押されました");
             playableDirector.Play();
+
+            /* Playerの角度も取得して、（カメラも位置と角度）
+             * Timelineの再生が終わったら
+             * 黄色の遷移的なのが始まって明るくなるころには戻っている
+             */
+            //transform.position = PlayerPosition;
+            //transform.eulerAngles = PlayerRotation;
+
+
         }   
     }
 
-    /*public void Start()
+    void OnEnable()
     {
-        this.playableDirector = this.GetComponent<PlayableDirector>();   
+        //OnEnable　この関数はオブジェクトが有効/アクティブになったときに呼び出されます。
+        playableDirector.stopped += OnPlayableDirectorStopped;
     }
 
-    //イベント再生メソッド
-    void EventPlay(int id)
+    /// <summary>
+    /// Timelineが終わったかどうかを検知する
+    /// </summary>
+    /// <param name="aDirector"></param>
+    void OnPlayableDirectorStopped(PlayableDirector aDirector)
     {
-        switch (id)
+        if (playableDirector == aDirector)
         {
-            case 1:
-                //再生したいTimelineをPlayableDirectorに再生させる
-                this.playableDirector.Play();
-                break;
-            //case 2:
+            Debug.Log("PlayableDirectorは、" + aDirector.name + "今停止した");
+
+            transform.position = PlayerPosition;
+            Debug.Log("Playerの現在の位置は" + PlayerPosition);
+            transform.eulerAngles = PlayerRotation;
+            Debug.Log("Playerの現在の角度は" + PlayerRotation);
         }
-    }*/
+    }
+
+    void OnDisable()
+    {
+        //コンポーネントが非アクティブになったときに呼ばれる
+        playableDirector.stopped -= OnPlayableDirectorStopped;
+    }
 
 }
