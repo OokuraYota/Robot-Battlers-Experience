79bd0265f9f5cd4a8b3e0a96b1ab666a41804046
OokuraYota <0912yota0820@gmail.com>
2021-01-18 04:29:52 +0900

    2021/01/18 4:27 ・AnimatorControllerのステート遷移時に音を出す方法として、AudioSource.PlayClipAtPoint( 音 ,  音を鳴らす場所)で県の音を鳴らす方法にした。 ・TODO Attack１・２・３で音を変えるようにしたい

--- Assets/Scripts/AttackCombo.cs
+++ Assets/Scripts/AttackCombo.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class AttackCombo : StateMachineBehaviour
 {
+    public AudioClip audioClip;
+
+    //public AudioClip sound1;
+    //public GameObject gameObject;
+    //public AudioSource audioSource;
+    //public AudioSord audioSord;
+
+    
+
     //Unity連続攻撃、コンボ攻撃アニメーションを実現する方法
+
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //AudioSource.PlayClipAtPoint(audioClip, animator.gameObject.transform.position);
+
         animator.SetBool("Attack", false);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            AudioSource.PlayClipAtPoint(audioClip, animator.gameObject.transform.position);
+        }
+
+
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //AudioSource.PlayClipAtPoint(audioClip, animator.gameObject.transform.position);
         if (Input.GetMouseButtonDown(0))//マウスの左クリックを押したら
         {
+
+
             animator.SetBool("Attack", true);
+
+ 
+
         }
     }
 
---
+++ Assets/Scripts/AudioSord.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AudioSord : MonoBehaviour
+{
+    public AudioClip sound1;
+    AudioSource audioSource;
+
+    public void Start()
+    {
+        //Componetを取得
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Update()
+    {
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            audioSource.PlayOneShot(sound1);
+        }*/
+
+    }
+
+    public void AudioSoud1()
+    {
+        audioSource.PlayOneShot(sound1);
+    }
+}
