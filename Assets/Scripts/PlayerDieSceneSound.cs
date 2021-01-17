cf7c3b2ed6ed1971fe8878dc00b65c743ec4d068
OokuraYota <0912yota0820@gmail.com>
2021-01-18 07:30:02 +0900

    PlayerDieSceneのPlayerをStartで爆発＆サウンドを追加

---
+++ Assets/Scripts/PlayerDieSceneSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDieSceneSound : MonoBehaviour
+{
+    public AudioClip AudioClip;
+
+    private void Start()
+    {
+        AudioSource.PlayClipAtPoint(AudioClip, this.gameObject.transform.position);
+    }
+}
