67cf0891a30b45f616abf8e6396f0c4a884e7f9c
OokuraYota <0912yota0820@gmail.com>
2021-01-29 08:43:05 +0900

    2021/01/29 8:40 ・PlayerDieSceneに遷移した際に、数秒経過したら『YOULOST』が表示されるように実装した。（良い感じに爆発が見えてから） ・Textに影を付けた。 ・これから、DisplayOnTheScreen()に入ったら、TextAnimationを独自で作成して、それを再生出来るようにしたい。 ・再生し終えたら、PlayerDieNovelSceneに行けるように秒数を調節する。

---
+++ Assets/Scripts/YOULOSTScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YOULOSTScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject TextY;
+    [SerializeField]
+    private GameObject TextO;
+    [SerializeField]
+    private GameObject TextU;
+    [SerializeField]
+    private GameObject TextL;
+    [SerializeField]
+    private GameObject TextO2;
+    [SerializeField]
+    private GameObject TextS;
+    [SerializeField]
+    private GameObject TextT;
+
+    public void Start()
+    {
+        //『YOULOST』を爆発がちゃんと見えるように非アクティブにしておく
+        TextY.SetActive(false);
+        TextO.SetActive(false);
+        TextU.SetActive(false);
+        TextL.SetActive(false);
+        TextO2.SetActive(false);
+        TextS.SetActive(false);
+        TextT.SetActive(false);
+
+        //コルーチンを実装
+        StartCoroutine("WaitingTime");
+
+        //自分のゲームオブジェクトを非アクティブに切り替える
+        //this.gameObject.SetActive(false);
+
+        //コルーチンを実装
+        //ゲームオブジェクト「GameTextManager」が非アクティブであるため
+        //コルーチン「WaitingTime」を開始できませんでした。
+        //StartCoroutine("WaitingTime");
+    }
+
+    public IEnumerator WaitingTime()
+    {
+        //0.7秒待ったら
+        yield return new WaitForSeconds(1.42f);
+        Debug.Log("2.5秒待ったら");
+
+        DisplayOnTheScreen();
+    }
+
+    public void DisplayOnTheScreen()
+    {
+        Debug.Log("爆発が見終わったので、画面に文字を表示");
+        TextY.SetActive(true);
+        TextO.SetActive(true);
+        TextU.SetActive(true);
+        TextL.SetActive(true);
+        TextO2.SetActive(true);
+        TextS.SetActive(true);
+        TextT.SetActive(true);
+    }
+}
