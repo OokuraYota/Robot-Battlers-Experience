c0888a52dd71fe36348fa379f4493a0492782e0a
OokuraYota <0912yota0820@gmail.com>
2021-02-19 02:29:58 +0900

    2021/02/19 2:29 ・ハリケーンの移動をScriptで管理

---
+++ Assets/Scripts/SpecialHurricaneManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 必殺技【ハリケーン】の管理
+/// </summary>
+public class SpecialHurricaneManager : MonoBehaviour
+{
+    /// <summary>
+    /// 変化量
+    /// </summary>
+    [SerializeField]
+    public float amountOfChange;
+
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        HurricaneMove();
+    }
+
+    /// <summary>
+    /// 必殺技ハリケーンの移動
+    /// </summary>
+    public void HurricaneMove()
+    {
+        //Time.deltaTimeは1フレーム前からの経過時間を表してくれているから
+        //変化量*時間の形にすることが出来た。
+        //これで、時間単位で移動してくれるようになった。
+
+        //Z方向（奥に行ってほしいから）
+        this.gameObject.transform.position += new Vector3(0,0,amountOfChange * Time.deltaTime);
+    }
+}
