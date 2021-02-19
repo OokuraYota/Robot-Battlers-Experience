1925e68636726d713059171909fdecaf55e61a39
OokuraYota <0912yota0820@gmail.com>
2021-02-19 23:35:11 +0900

    2021/02/19 23:34 ・Titleの遷移の際、ボタンが鳴ってから遷移するようにした。 ・使用していないScriptを削除した。

--- Assets/Scripts/ButtonClickSound.cs
+++ Assets/Scripts/ButtonClickSound.cs
@@ -1,22 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Buttonを押したときのSound
 /// </summary>
 public class ButtonClickSound : MonoBehaviour
 {
-    private AudioSource STRAT;
+    /// <summary>
+    /// ボタンを押したらなる音源
+    /// </summary>
+    private AudioSource soundSource;
+
 
     void Start()
     {
-        STRAT = GetComponent<AudioSource>();
+        soundSource = GetComponent<AudioSource>();
     }
 
     public void OnClick()
     {
-        STRAT.PlayOneShot(STRAT.clip);
+        //soundSource.PlayOneShot(soundSource.clip);
+
+        //2021 02 19
+        soundSource.Play();
+
+        StartCoroutine(Checking(() =>
+        {
+            Debug.Log("END");
+
+            StartTransition();
+            //SceneManager.LoadScene("ViewoftheworldScene");
+        }));
+    }
+
+    public delegate void functionType();
+
+    private IEnumerator Checking(functionType callback)
+    {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+            if (!soundSource.isPlaying)
+            {
+                callback();
+                break;
+            }
+        }
+    }
+
+    public void StartTransition()
+    {
+        Debug.Log("遷移を始めます");
+        SceneManager.LoadScene("ViewoftheworldScene");
     }
 }
 
